a7678d71a6b94ee73c3862c54b4cc373f4092def
RemoteJJH <goodjob321@hanmail.net>
2024-09-23 23:28:41 +0900

    Initial commit

---
+++ src/test/java/company/space/recode/RecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package company.space.recode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
