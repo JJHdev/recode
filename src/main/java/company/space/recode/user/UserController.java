ca43076959e2d308ac5de7a4ad975e9fc41e5539
jjh <goodjob321@hanmail.net>
2024-10-12 02:36:38 +0900

    계정 로그인, 회원가입 화면 작성완료

--- src/main/java/company/space/recode/user/UserController.java
+++ src/main/java/company/space/recode/user/UserController.java
@@ -5,6 +5,7 @@ import company.space.recode.hello.HelloUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,8 +18,19 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @GetMapping("/login")
     public String userLogin(Model model) {
+        return "user/login";
+    }
+
+    @GetMapping("/regiUser")
+    public String openRegiUser(Model model) {
+        return "user/regiUser";
+    }
+
+    @PostMapping("/regiUser")
+    public String regiUser(Model model) {
         HelloUser user = new HelloUser();
         user.setUserId("admin");
         user.setUserName("이름입력");
@@ -26,7 +38,7 @@ public class UserController {
         user.setGender("M");
         user.setEmail("goodjob321@hanmail.net");
         user.setStatus("ACTIVE");
-        //userService.sayHello(user);
-        return "user/login";
+        System.out.printf("123123");
+        return "user/regiUser";
     }
 }
